abdeecf753e0cf063e33e1a578baa570e64fd9b7
Sunita <sunitamousaviii@gmail.com>
2025-09-13 19:47:28 +0330

    Install React and Bootstrap

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App ">
+      <footer className="App-footer text-center">
+        Coded by{" "}
+        <a
+          href="https://github.com/SunitaMousavi"
+          target="_blank"
+          rel="noopener noreferrer">
+          Sunita Mousavi
+        </a>
+        , open-sourced on{" "}
+        <a
+          href="https://github.com/SunitaMousavi/react-weather"
+          target="_blank"
+          rel="noopener noreferrer">
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://reactweatherapp-sunitamousavi.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer">
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
