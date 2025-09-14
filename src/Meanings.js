5f12b69c12f2c412f462c3739b3426ee0de555b1
Sunita <sunitamousaviii@gmail.com>
2025-09-14 16:58:27 +0330

    Rendered definitions and meanings

---
+++ src/Meanings.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Meanings(props) {
+  return (
+    <div className="Meanings">
+      <h3>{props.meaning.partOfSpeech}</h3>
+
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>{definition.definition}</p>
+            <p>
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
