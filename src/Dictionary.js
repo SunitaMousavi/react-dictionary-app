9508fafa51c4a61ef29285912f29860dd254b427
Sunita <sunitamousaviii@gmail.com>
2025-09-14 17:53:14 +0330

    Rendered synonyms

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,13 +8,12 @@ export default function Dictionary() {
   let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
   }
 
   function Search(event) {
     event.preventDefault();
-    
+
     // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
---
+++ src/Synonyms.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  if (props.synonyms) {
+    return (
+      <div className="Synonyms">
+        <ul>
+          {props.synonyms.map(function (synonym, index) {
+            return (
+              <li key={index} className="synonym">
+                {synonym}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
